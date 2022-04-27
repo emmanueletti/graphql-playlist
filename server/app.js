89f7abdddecb86afdd18229566daba4272717fef
Emmanuel Etti <emmanueletti@gmail.com>
2022-04-26 21:41:04 -0700

    saving progress

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = 3000;
 
 // graphql is set up as an express middleware with an endpoint that acts as a supercharged entry
 // point for all graph ql requests
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, () => {
   console.log(`Server listing on port ${PORT}`);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,25 +1,56 @@
 const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const _ = require('lodash');
 
+// dummy data
+const books = [
+  { name: 'Name of the Wind', genre: 'Drama', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Redwall', genre: 'Fantasy', id: '4' },
+];
 // the schema describes the relationship and shape of our data which tells
 // graphql how to move through our data and how each point on of our data connects
 
 // aka this schema file has two functions:
 // 1 - to define the shape of our data - "types"
 // 2 - to define how our data connects to each other - "relationships"
-// 3 - to define the root queries - the points in which requests can jump into
+// 3 - to define the root queries - the points in which front end requests can jump into
 //      our data graph
 
-const { GraphQLObjectType, GraphQLString } = graphql;
-
+// defines shape of the graphql data
+// think of this as a model in Rails
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    // id: { type: GraphQLString }, // type uses maybe graphQL enums to describe the datatype of each field
+    id: { type: GraphQLID }, // using the graphql type id for increased flexibility, id can now be sent in the request as an int or string
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
 });
 
-const rootQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
+  // fields of root queries dont need a function value for the fields key
+  fields: {
+    // first root query is for a single book, the naming of the key is very important
+    // graphQL will interpolate the name we give into the resulting db requests it makes
+    book: {
+      type: BookType,
+      // define the arguements that the front end needs to pass along with this root request
+      // args: { id: { type: GraphQLString } }, // args object that will be passed into the resolve function
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // resolve function is to get data from db / other source
+        // the function fires when a request to this "book" query is received
+        // args param gives us access to any args passed in by the user e.g args.id
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
